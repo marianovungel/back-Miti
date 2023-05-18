27f743793b27ab4396aa459954b8c0c41581509d
Marisa-Basilio <edna189@hotmail.com>
2023-05-18 13:10:11 -0300

    adm

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     nome:String,
     senha: String,
+    adm: Boolean,
     pontoC: Number,
     pontoJava: Number,
     pontoJS: Number,
